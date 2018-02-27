da0f57ecc01f1e153cd5ce3ee4ac9b16e820ab31
Vinicius Guedes <viniciusgued@gmail.com>
2018-02-27 19:12:20 -0300

    feature(types): adds an index.d.ts file

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,14 @@ import { StackerJS } from "stackerjs-types";
 declare module "stackerjs-db-mysql-adapter"
 {
 
+    export class QueryBuilder extends StackerJS.DB.QueryBuilder
+    {
+
+    }
+
+    export class QueryCriteria extends StackerJS.DB.QueryCriteria
+    {
+
+    }
 
 }
